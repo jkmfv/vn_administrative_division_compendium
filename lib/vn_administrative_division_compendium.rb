7c86bd7fd858ca6d6befe15be31239b1e8fcadef
jkmfv <tran.van.dat@moneyforward.vn>
2023-12-31 14:24:00 +0700

    bundle gem

---
+++ lib/vn_administrative_division_compendium.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vn_administrative_division_compendium/version"
+
+module VnAdministrativeDivisionCompendium
+  class Error < StandardError; end
+  # Your code goes here...
+end
